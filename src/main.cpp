436d276e4d49ec9c04f53f4fd87b04c993d33770
oaleksander <medvedev.asm@outlook.com>
2021-09-26 16:53:24 +0300

    Cool setup thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,28 @@
 #include <Arduino.h>
 #include "WireNoFreeze.h"
-#include "SPI.h" //Does not compile without this magic line...
-#include "Adafruit_TCS34725.h"
-#include "Adafruit_BNO055.h"
-#include "Prizm.h"
+#include "prizm.h"
 
 
-
-    Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
-
-    Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
-
-    Prizm prizm;
-
-    DCExpansion drivetrainMotorExpansion1 = DCExpansion(1);
-    DCExpansion drivetrainMotorExpansion2 = DCExpansion(2);
-    DCExpansion intakeMotorExpansion2 = DCExpansion(3);
-    DCExpansion ledStripMotorExpansion = prizm.integratedDC;
+#include "barrier.h"
+#include "drivetrain.h"
+#include "intake.h"
+#include "ledstrip.h"
 
 void setup() {
+    //initialization phase 1
     prizm.prizmBegin();
+    //initalization phase 2
+    barrier.init();
+    drivetrain.init();
+    intake.init();
+    led_strip.init();
+
+    prizm.setRedLED(HIGH);                    // Turn on when we're reset
+    while (prizm.readStartButton() == 0) {
+        prizm.setGreenLED(millis() % 333 < 166 ? HIGH : LOW);
+    }        // wait for the program start (green) button pressed
+    prizm.setGreenLED(LOW);
+    prizm.setRedLED(LOW);
 }
 
 void loop() {
